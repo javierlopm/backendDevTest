bf6f52be72790888fec2a15d5213e24bda14f77b
Javier <javierloplom@gmail.com>
2023-07-02 22:31:19 +0200

    controller example

---
+++ src/src/main/java/portfolio/src/application/controller/ProductController.java
@@ -0,0 +1,19 @@
+package portfolio.src.application.controller;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/product")
+public class ProductController {
+
+    @GetMapping("/{productId}/similarids")
+    public ResponseEntity<String> getSimilarIds(@PathVariable String productId) {
+        String message = "Hello, World!" + productId;
+        return ResponseEntity.ok(message);
+    }
+}
